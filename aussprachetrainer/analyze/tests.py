7ddf008e489d5a49f8834247336f687f65931155
Daniel Busch <dakopen185@gmail.com>
2023-08-28 20:55:07 +0200

    moved analysis test to the right place

--- aussprachetrainer/analyze/tests.py
+++ aussprachetrainer/analyze/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from analyze.pronunciation_assessment import pronunciation_assessment_continuous_from_file
 
-# Create your tests here.
+class AnalysisTest(TestCase):
+    
+    def test_pronunciation_assessment_continuous_from_file(self):
+        result = pronunciation_assessment_continuous_from_file(
+            'static/frontend/assets/testaudio/ich-habe-etwas-mehr-durst-als-ich-vor-einer-stunde-noch-hatte.wav',                                                   
+            'Ich habe etwas mehr Durst, als ich vor einer Stunde noch hatte.',
+            'de-DE'
+        )
+        self.assertEqual([word["word"] for  word in result['Words']], ['Ich', 'habe', 'etwas', 'mehr', 'Durst', 'als', 'ich', 'vor', 'einer', 'Stunde', 'noch', 'hatte'])
+
+        for score in [result['Paragraph']['accuracy_score'], result['Paragraph']['completeness_score'], result['Paragraph']['fluency_score']]:
+            self.assertTrue(score >= 80 and score <= 100) # the ML model can change but the scores should be in this range
--- aussprachetrainer/frontend/tests.py
+++ aussprachetrainer/frontend/tests.py
@@ -3,7 +3,6 @@ from django.urls import reverse
 from django.utils import translation
 from django.core.management import call_command
 import os.path
-from analyze.pronunciation_assessment import pronunciation_assessment_continuous_from_file
 
 class FrontendTests(TestCase):
     
@@ -29,15 +28,3 @@ class MultiLanguageTestCase(TestCase):
         if not mo_already_existed:
             os.remove('frontend/locale/en/LC_MESSAGES/django.mo')
 
-class AnalysisTest(TestCase):
-    
-    def test_pronunciation_assessment_continuous_from_file(self):
-        result = pronunciation_assessment_continuous_from_file(
-            'static/frontend/assets/testaudio/ich-habe-etwas-mehr-durst-als-ich-vor-einer-stunde-noch-hatte.wav',                                                   
-            'Ich habe etwas mehr Durst, als ich vor einer Stunde noch hatte.',
-            'de-DE'
-        )
-        self.assertEqual([word["word"] for  word in result['Words']], ['Ich', 'habe', 'etwas', 'mehr', 'Durst', 'als', 'ich', 'vor', 'einer', 'Stunde', 'noch', 'hatte'])
-
-        for score in [result['Paragraph']['accuracy_score'], result['Paragraph']['completeness_score'], result['Paragraph']['fluency_score']]:
-            self.assertTrue(score >= 80 and score <= 100) # the ML model can change but the scores should be in this range
