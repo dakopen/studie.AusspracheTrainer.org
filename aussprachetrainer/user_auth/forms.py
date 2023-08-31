d0c1b57e7cbfccfca9847ea1795592cd51deee9c
Daniel Busch <dakopen185@gmail.com>
2023-08-31 14:26:59 +0200

    no collection of gender and age anymore

--- aussprachetrainer/user_auth/forms.py
+++ aussprachetrainer/user_auth/forms.py
@@ -1,22 +1,11 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
 from .models import CustomUser
 from django.utils.translation import gettext as _
 
-GENDER_CHOICES = [
-    ("m", _("männlich")),
-    ("f", _("weiblich")),
-    ("d", _("divers")),
-]
-
-
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField(label=_("E-Mail"), required=True)
-    age = forms.IntegerField(min_value=0, max_value=142, label=_("Alter"), required=False)
-    gender = forms.ChoiceField(choices=GENDER_CHOICES, label=_("Geschlecht"), required=False)
-    
+    email = forms.EmailField(label=_("E-Mail"), required=True)    
     class Meta:
         model = CustomUser
-        fields = ["username", "email", "age", "gender", "password1", "password2"]
+        fields = ["username", "email", "password1", "password2"]
     
