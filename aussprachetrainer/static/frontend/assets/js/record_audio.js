c5765c8451238de71f48cd262742a6aa89c5ee1a
Daniel Busch <dakopen185@gmail.com>
2023-10-26 17:18:27 +0200

    ADD: language selection

--- aussprachetrainer/static/frontend/assets/js/record_audio.js
+++ aussprachetrainer/static/frontend/assets/js/record_audio.js
@@ -51,6 +51,83 @@ recordButton.on('click', function() {
         recordButton.text("Click me!");
     }
 });
+// START: Dropdown menu //
+/*
+function toggleDropdown() {
+    const dropdown = document.getElementById("languageDropdown");
+    const dropbtn = document.querySelector('.dropbtn');
+    const currentLang = document.querySelector('.dropbtn img').getAttribute('data-lang');
+    
+    // Hide the current flag from dropdown
+    document.querySelectorAll('#languageDropdown img').forEach(img => {
+        img.style.display = img.getAttribute('data-lang') === currentLang ? 'none' : 'block';
+    });
 
+    if (dropdown.style.display === 'block') {
+        dropdown.style.display = 'none';
+        dropbtn.classList.remove('active');
+    } else {
+        dropdown.style.display = 'block';
+        dropbtn.classList.add('active');
 
-//
+    }
+}
+
+// Update the button to display the selected flag
+document.getElementById("languageDropdown").addEventListener('click', function(e) {
+    e.preventDefault();
+    const btnImg = document.querySelector('.dropbtn img');
+    if (e.target.tagName === 'IMG') {
+        btnImg.src = e.target.src;
+        btnImg.alt = e.target.alt;
+        btnImg.width = e.target.width;  // Preserve width
+        btnImg.height = e.target.height;  // Preserve height
+        btnImg.setAttribute('data-lang', e.target.getAttribute('data-lang'));  // Update data-lang
+        toggleDropdown();
+    }
+});
+*/
+/*Dropdown Menu*/
+let selectedLiId = 'dropdown-lang-english';
+$('#' + selectedLiId).hide();
+
+$('.dropdown').click(function () {
+    $(this).attr('tabindex', 1).focus();
+    $(this).toggleClass('active');
+    $(this).find('.dropdown-menu').slideToggle(300);
+
+    // Do not show the already selected flag
+    $('#' + selectedLiId).hide();
+});
+$('.dropdown').focusout(function () {
+    $(this).removeClass('active');
+    $(this).find('.dropdown-menu').slideUp(300);
+});
+$('.dropdown .dropdown-menu li').click(function () {
+    // show the old selected flag again
+    let tmpSelection = selectedLiId;
+    setTimeout(() => {
+        $('#' + tmpSelection).show();
+    }, 301);
+    
+    // set the id to the new selected flag
+    selectedLiId = $(this).attr('id');
+
+    var selectedText = $(this).clone().children().remove().end().text(); // Entfernt die Flagge und holt nur den Text
+    var selectedFlag = $(this).find('.dropdown-flag').clone(); // Klont das Flaggenbild
+    selectedFlag.height(20).width(30);
+
+    $(this).parents('.dropdown').find('.dropdown-select > span').text(selectedText).append(selectedFlag); // Fügt zuerst das Bild und dann den Text hinzu
+    $(this).parents('.dropdown').find('input').attr('value', $(this).attr('id'));
+
+});
+/*
+$('.dropdown .dropdown-menu li').click(function () {
+    $(this).parents('.dropdown').find('span').text($(this).text());
+    $(this).parents('.dropdown').find('input').attr('value', $(this).attr('id'));
+});
+*/
+/*End Dropdown Menu*/
+
+// END: Dropdown menu //
+
